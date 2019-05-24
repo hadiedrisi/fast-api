421bcea9c6d119be78cf67fe7fe3800bf613e904
hadi edrisi <hadi.mhe@gmail.com>
2019-05-24 03:18:49 -0700

    add fastify server

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mycargarage')
+	.then(()=>{
+	console.log('mongoDB Connected...');	
+	})
+	.catch(err=>{
+	console.log(err);
+	})
+const fastify = require('fastify')({
+
+	logger:true
+});
+
+fastify.get('/',async(request,reply)=>{
+
+	return { hello:'world'}
+});
+
+
+const start = async()=>{
+
+	try{
+
+	await fastify.listen(3000);
+	fastify.log.info('server listening on ${fastify.server.address().port}');
+	
+	}
+	catch(err){
+	fastify.log.error(err)
+	process.exit(1)
+	}
+
+}
+
+start()
